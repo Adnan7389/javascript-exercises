e7638e920a1948d6ea747a49ede0539db8c38657
adnan7389 <adnannuredin7389@gmail.com>
2024-10-05 23:02:53 +0300

    solution for The Odin javascript calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let sum = 0;
+  sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let difference = a - b;
+  return difference
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+
+  if (array.length === 0) return sum;
+  else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  }
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let product = 1
 
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let power = a ** b;
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
