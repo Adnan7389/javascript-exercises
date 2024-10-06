a655b879a78501efe2964c4df185f693ddb1036b
adnan7389 <adnannuredin7389@gmail.com>
2024-10-06 07:11:05 +0300

    solution for The Odin Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.toLowerCase();
 
+  str = str.replace(/[^a-z0-9]/g, '')
+
+  let reversed = str.split('').reverse().join('');
+  return reversed === str;
 };
 
 // Do not edit below this line
