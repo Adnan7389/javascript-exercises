7e62ae7797802634a4c8a23ee0bca6534b37d5ff
adnan7389 <adnannuredin7389@gmail.com>
2024-10-09 10:43:28 +0300

    solution for fibonacci exercize

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
 
+  if (n < 0) return "OOPS";
+
+  n = parseInt(n);
+
+  let a = 0, b = 1, temp;
+
+  for (let i = 2; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return n === 0 ? 0 : b;
 };
 
 // Do not edit below this line
